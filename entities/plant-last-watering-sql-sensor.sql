1e7fdcf0b65202565c844027f533d421030668d3
jorgeramira <jorge.ramira@gmail.com>
2025-06-21 01:42:19 +0400

    Update plant-last-watering-sql-sensor.sql

--- entities/plant-last-watering-sql-sensor.sql
+++ entities/plant-last-watering-sql-sensor.sql
@@ -11,7 +11,7 @@ SELECT s.state, datetime(last_updated_ts, 'unixepoch') as last_updated,
          LAG(s.state) OVER (ORDER BY s.last_updated_ts) AS previous_state
   FROM states s
   JOIN states_meta sm ON sm.metadata_id = s.metadata_id
-  WHERE sm.entity_id = 'sensor.maia_soil_moisture'
+  WHERE sm.entity_id = 'sensor.{plant}_soil_moisture' --replace here with the corresponding sensor for the plant
     AND s.state NOT IN ('unknown', 'unavailable')
 ) s
 WHERE  (CAST(s.state AS FLOAT) - CAST(s.previous_state AS FLOAT)) > 10
