a72d2398a4b8f8fd80c161bbcc704bbfca4b5e4b
jorgeramira <jorge.ramira@gmail.com>
2025-06-21 01:41:30 +0400

    Create plant-last-watering-sql-sensor.sql

---
+++ entities/plant-last-watering-sql-sensor.sql
@@ -0,0 +1,19 @@
+SELECT days_elapsed
+FROM (
+  SELECT 
+    s.last_updated, 
+    s.state, 
+    s.previous_state, 
+    (CAST(s.state AS FLOAT) - CAST(s.previous_state AS FLOAT)) AS delta,
+    ROUND((julianday('now') - julianday(s.last_updated)), 2) AS days_elapsed
+  FROM (
+SELECT s.state, datetime(last_updated_ts, 'unixepoch') as last_updated,
+         LAG(s.state) OVER (ORDER BY s.last_updated_ts) AS previous_state
+  FROM states s
+  JOIN states_meta sm ON sm.metadata_id = s.metadata_id
+  WHERE sm.entity_id = 'sensor.maia_soil_moisture'
+    AND s.state NOT IN ('unknown', 'unavailable')
+) s
+WHERE  (CAST(s.state AS FLOAT) - CAST(s.previous_state AS FLOAT)) > 10
+ORDER BY s.last_updated DESC
+LIMIT 1)
