e1fd8044e5f85130dfa7488ddd42f3c291455b02
jorgeramira <jorge.ramira@gmail.com>
2025-11-23 12:01:15 +0400

    Update plant-last-watering-sql-sensor.sql

--- entities/plant-last-watering-sql-sensor.sql
+++ entities/plant-last-watering-sql-sensor.sql
@@ -1,19 +1,23 @@
 SELECT days_elapsed
 FROM (
   SELECT 
-    s.last_updated, 
-    s.state, 
-    s.previous_state, 
-    (CAST(s.state AS FLOAT) - CAST(s.previous_state AS FLOAT)) AS delta,
-    ROUND((julianday('now') - julianday(s.last_updated)), 2) AS days_elapsed
+    s.created_ts,
+    datetime(s.created_ts, 'unixepoch') as created_datetime, 
+    s.mean, 
+    s.previous_mean, 
+    (s.mean - s.previous_mean) AS delta,
+    ROUND((julianday('now') - julianday(datetime(s.created_ts, 'unixepoch'))), 2) AS days_elapsed
   FROM (
-SELECT s.state, datetime(last_updated_ts, 'unixepoch') as last_updated,
-         LAG(s.state) OVER (ORDER BY s.last_updated_ts) AS previous_state
-  FROM states s
-  JOIN states_meta sm ON sm.metadata_id = s.metadata_id
-  WHERE sm.entity_id = 'sensor.{plant}_soil_moisture' --replace here with the corresponding sensor for the plant
-    AND s.state NOT IN ('unknown', 'unavailable')
-) s
-WHERE  (CAST(s.state AS FLOAT) - CAST(s.previous_state AS FLOAT)) > 10
-ORDER BY s.last_updated DESC
-LIMIT 1)
+    SELECT 
+      st.mean, 
+      st.created_ts,
+      LAG(st.mean) OVER (ORDER BY st.created_ts) AS previous_mean
+    FROM statistics st
+    JOIN statistics_meta sm ON sm.id = st.metadata_id
+    WHERE sm.statistic_id = 'sensor.{plant}_soil_moisture' --replace here with the corresponding sensor for the plant
+      AND st.mean IS NOT NULL
+  ) s
+  WHERE (s.mean - s.previous_mean) > 5
+  ORDER BY s.created_ts DESC
+  LIMIT 1
+)
